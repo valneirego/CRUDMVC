b68e95c1a8633287ece5a221bc6c5764d928164f
Valnei Rego <valneinfo@gmail.com>
2021-12-02 23:59:16 -0300

    Testando o controller e Páginas Razor

--- crudMVC/Controllers/HomeController.cs
+++ crudMVC/Controllers/HomeController.cs
@@ -17,7 +17,9 @@ namespace crudMVC.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Sales Web MVC App from C# Course";
+            ViewData["Students"] = "Valnei Lima Rego";
+            ViewData["Email"] = "valneinfo@gmail.com";
 
             return View();
         }
