6220a68625a13b11aba046a89b9b16e8abf8a67a
Valnei Rego <valneinfo@gmail.com>
2021-12-06 17:45:34 -0300

    Adapitação para MySQL e a primeira migração

---
+++ crudMVC/Data/crudMVCContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace crudMVC.Models
+{
+    public class crudMVCContext : DbContext
+    {
+        public crudMVCContext (DbContextOptions<crudMVCContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<crudMVC.Models.Department> Department { get; set; }
+    }
+}
--- crudMVC/Startup.cs
+++ crudMVC/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using crudMVC.Models;
 
 namespace crudMVC
 {
@@ -33,6 +35,11 @@ namespace crudMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddDbContext<crudMVCContext>(options =>
+                   
+            options.UseMySql(Configuration.GetConnectionString("crudMVCContext"), builder =>
+builder.MigrationsAssembly("crudMVC")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
